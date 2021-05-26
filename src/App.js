40184d5c1889c5833ecaadfa0c6f33013f8751f7
unknown <khahlem91@gmail.com>
2021-05-26 11:45:06 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,40 @@
-import logo from './logo.svg';
 import './App.css';
+import Movielist from './component/Movielist';
+import {useState} from 'react';
+import Movieadd from './component/Movieadd';
 
 function App() {
+  const [films, setFilms] = useState([
+    {
+      title : "Game of thrones",
+      description : " Genre: Fantasty",
+      posterUrl :"https://m.media-amazon.com/images/M/MV5BYTRiNDQwYzAtMzVlZS00NTI5LWJjYjUtMzkwNTUzMWMxZTllXkEyXkFqcGdeQXVyNDIzMzcwNjc@._V1_.jpg ",
+      rate :"4",
+    } ,
+    {
+      title : "Train To Busan",
+      description : " Genre: Action horror",
+      posterUrl :"https://upload.wikimedia.org/wikipedia/ar/9/95/Train_to_Busan.jpg",
+      rate :"5",
+    },
+    {
+      title : "the boy",
+      description : " Genre: horror",
+      posterUrl :"https://i.pinimg.com/originals/81/dd/72/81dd7292396ed6edf218e0bb9d354f8a.jpg ",
+      rate :"3",
+    },
+    
+  ])
+  const add=(newMovie)=>{
+    setFilms([...films,newMovie])
+
+  }
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Movielist films={films}/>
+        <Movieadd add={add}/>
+
       </header>
     </div>
   );
